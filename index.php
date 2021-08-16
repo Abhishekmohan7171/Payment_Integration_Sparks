f146a2c92960bfcf30da566daef71f320e49f01c
Abhishekmohan7171 <abhishekmohan7171@gmail.com>
2021-08-16 11:33:50 +0530

    error cleared

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="rgba(0, 0, 255, 0.5);">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="#!">Sparks Foundation</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
